2909961c174f16799bd8ccc80c75fb65b8605c46
abuhumayed <ahmedthanjim@hotmail.co.uk>
2023-10-18 15:39:55 +0100

    toggle updates

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-function toggleMenu()
+
+// window.onload = () =>
+// {
+//   // function toggleMenu()
+//   // {
+//   //   const menu = document.querySelector(".menu-links");
+//   //   const icon = document.querySelector(".hamburger-icon");
+//   //   menu.classList.toggle("open")
+//   //   icon.classList.toggle("open")
+
+//   // }
+// }
+
+
+
+
+
+window.onload = () =>
 {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open")
-  icon.classList.toggle("open")
+  document.querySelector(".menu-links").onclick = function() {toggleMenu()};
+  document.querySelector(".hamburger-icon").onclick = function() {toggleMenu()};
+
+  function toggleMenu()
+  {
+   
+    document.querySelector(".menu-links").classList.toggle("open")
+   
+    document.querySelector(".hamburger-icon").classList.toggle("open")
+   
+  }
 
 }
